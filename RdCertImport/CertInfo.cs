4fd7497a879ecb00e20414c958d0dda794078322
ColinZeb <Colin@mmin.us>
2016-11-24 15:49:09 +0800

    补上遗漏文件

---
+++ RdCertImport/CertInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace RdCertImport
+{
+    public class CertInfo
+    {
+        public string FriendlyName { get; set; }
+        public string Hash { get; internal set; }
+        public string IssuerName { get; set; }
+        public string Subject { get; set; }
+        public string SubjectName { get; set; }
+        public bool Useable { get; set; }
+    }
+}
