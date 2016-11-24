4868e7a3dc2981a662b38cc6e6eec58ffefa67d0
ColinZeb <Colin@mmin.us>
2016-11-24 09:41:39 +0800

    添加项目文件。

---
+++ RdCertImport/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace RdCertImport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialSingleLineTextField2 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.mfbChoice = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialSingleLineTextField1 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.certHash = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialRaisedButton2 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // materialTabSelector1
+            // 
+            this.materialTabSelector1.BaseTabControl = this.materialTabControl1;
+            this.materialTabSelector1.Depth = 0;
+            this.materialTabSelector1.Location = new System.Drawing.Point(3, 67);
+            this.materialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabSelector1.Name = "materialTabSelector1";
+            this.materialTabSelector1.Size = new System.Drawing.Size(802, 77);
+            this.materialTabSelector1.TabIndex = 0;
+            this.materialTabSelector1.Text = "materialTabSelector1";
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.Controls.Add(this.tabPage1);
+            this.materialTabControl1.Controls.Add(this.tabPage2);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Location = new System.Drawing.Point(3, 150);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(802, 347);
+            this.materialTabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.materialFlatButton2);
+            this.tabPage1.Controls.Add(this.materialFlatButton1);
+            this.tabPage1.Controls.Add(this.materialLabel2);
+            this.tabPage1.Controls.Add(this.materialLabel1);
+            this.tabPage1.Controls.Add(this.materialSingleLineTextField2);
+            this.tabPage1.Controls.Add(this.mfbChoice);
+            this.tabPage1.Controls.Add(this.materialRaisedButton1);
+            this.tabPage1.Controls.Add(this.materialSingleLineTextField1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(794, 321);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "导入证书";
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Location = new System.Drawing.Point(420, 121);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(72, 36);
+            this.materialFlatButton1.TabIndex = 6;
+            this.materialFlatButton1.Text = "验证密码";
+            this.materialFlatButton1.UseVisualStyleBackColor = true;
+            this.materialFlatButton1.Click += new System.EventHandler(this.materialFlatButton1_Click);
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(10, 144);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(41, 19);
+            this.materialLabel2.TabIndex = 5;
+            this.materialLabel2.Text = "密码";
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(6, 87);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(114, 19);
+            this.materialLabel1.TabIndex = 4;
+            this.materialLabel1.Text = "X509证书(.PFX)";
+            // 
+            // materialSingleLineTextField2
+            // 
+            this.materialSingleLineTextField2.Depth = 0;
+            this.materialSingleLineTextField2.Hint = "";
+            this.materialSingleLineTextField2.Location = new System.Drawing.Point(135, 134);
+            this.materialSingleLineTextField2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField2.Name = "materialSingleLineTextField2";
+            this.materialSingleLineTextField2.PasswordChar = '\0';
+            this.materialSingleLineTextField2.SelectedText = "";
+            this.materialSingleLineTextField2.SelectionLength = 0;
+            this.materialSingleLineTextField2.SelectionStart = 0;
+            this.materialSingleLineTextField2.Size = new System.Drawing.Size(244, 23);
+            this.materialSingleLineTextField2.TabIndex = 3;
+            this.materialSingleLineTextField2.UseSystemPasswordChar = false;
+            // 
+            // mfbChoice
+            // 
+            this.mfbChoice.AutoSize = true;
+            this.mfbChoice.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.mfbChoice.Depth = 0;
+            this.mfbChoice.Location = new System.Drawing.Point(420, 70);
+            this.mfbChoice.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.mfbChoice.MouseState = MaterialSkin.MouseState.HOVER;
+            this.mfbChoice.Name = "mfbChoice";
+            this.mfbChoice.Primary = false;
+            this.mfbChoice.Size = new System.Drawing.Size(42, 36);
+            this.mfbChoice.TabIndex = 2;
+            this.mfbChoice.Text = "选择";
+            this.mfbChoice.UseVisualStyleBackColor = true;
+            this.mfbChoice.Click += new System.EventHandler(this.mfbChoice_Click);
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Location = new System.Drawing.Point(528, 238);
+            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(227, 44);
+            this.materialRaisedButton1.TabIndex = 1;
+            this.materialRaisedButton1.Text = "导入并设置为远程桌面验证证书";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            this.materialRaisedButton1.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // materialSingleLineTextField1
+            // 
+            this.materialSingleLineTextField1.Depth = 0;
+            this.materialSingleLineTextField1.Hint = "";
+            this.materialSingleLineTextField1.Location = new System.Drawing.Point(135, 83);
+            this.materialSingleLineTextField1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField1.Name = "materialSingleLineTextField1";
+            this.materialSingleLineTextField1.PasswordChar = '\0';
+            this.materialSingleLineTextField1.SelectedText = "";
+            this.materialSingleLineTextField1.SelectionLength = 0;
+            this.materialSingleLineTextField1.SelectionStart = 0;
+            this.materialSingleLineTextField1.Size = new System.Drawing.Size(244, 23);
+            this.materialSingleLineTextField1.TabIndex = 0;
+            this.materialSingleLineTextField1.UseSystemPasswordChar = false;
+            this.materialSingleLineTextField1.Click += new System.EventHandler(this.materialSingleLineTextField1_Click);
+            this.materialSingleLineTextField1.TextChanged += new System.EventHandler(this.materialSingleLineTextField1_TextChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.White;
+            this.tabPage2.Controls.Add(this.materialRaisedButton2);
+            this.tabPage2.Controls.Add(this.materialLabel3);
+            this.tabPage2.Controls.Add(this.certHash);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(794, 321);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "已经导入";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // certHash
+            // 
+            this.certHash.Depth = 0;
+            this.certHash.Hint = "";
+            this.certHash.Location = new System.Drawing.Point(126, 72);
+            this.certHash.MouseState = MaterialSkin.MouseState.HOVER;
+            this.certHash.Name = "certHash";
+            this.certHash.PasswordChar = '\0';
+            this.certHash.SelectedText = "";
+            this.certHash.SelectionLength = 0;
+            this.certHash.SelectionStart = 0;
+            this.certHash.Size = new System.Drawing.Size(229, 23);
+            this.certHash.TabIndex = 0;
+            this.certHash.UseSystemPasswordChar = false;
+            // 
+            // materialLabel3
+            // 
+            this.materialLabel3.AutoSize = true;
+            this.materialLabel3.Depth = 0;
+            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel3.Location = new System.Drawing.Point(22, 72);
+            this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel3.Name = "materialLabel3";
+            this.materialLabel3.Size = new System.Drawing.Size(73, 19);
+            this.materialLabel3.TabIndex = 1;
+            this.materialLabel3.Text = "证书哈希";
+            // 
+            // materialRaisedButton2
+            // 
+            this.materialRaisedButton2.Depth = 0;
+            this.materialRaisedButton2.Location = new System.Drawing.Point(374, 72);
+            this.materialRaisedButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton2.Name = "materialRaisedButton2";
+            this.materialRaisedButton2.Primary = true;
+            this.materialRaisedButton2.Size = new System.Drawing.Size(99, 23);
+            this.materialRaisedButton2.TabIndex = 2;
+            this.materialRaisedButton2.Text = "设置";
+            this.materialRaisedButton2.UseVisualStyleBackColor = true;
+            this.materialRaisedButton2.Click += new System.EventHandler(this.materialRaisedButton2_Click);
+            // 
+            // materialFlatButton2
+            // 
+            this.materialFlatButton2.AutoSize = true;
+            this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton2.Depth = 0;
+            this.materialFlatButton2.Location = new System.Drawing.Point(528, 121);
+            this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton2.Name = "materialFlatButton2";
+            this.materialFlatButton2.Primary = false;
+            this.materialFlatButton2.Size = new System.Drawing.Size(109, 36);
+            this.materialFlatButton2.TabIndex = 7;
+            this.materialFlatButton2.Text = "复制证书hash";
+            this.materialFlatButton2.UseVisualStyleBackColor = true;
+            this.materialFlatButton2.Click += new System.EventHandler(this.materialFlatButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 509);
+            this.Controls.Add(this.materialTabControl1);
+            this.Controls.Add(this.materialTabSelector1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RD证书导入工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.materialTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialTabSelector materialTabSelector1;
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField1;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
+        private MaterialSkin.Controls.MaterialFlatButton mfbChoice;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+        private MaterialSkin.Controls.MaterialSingleLineTextField certHash;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton2;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton2;
+    }
+}
+
---
+++ RdCertImport/Form1.cs
@@ -0,0 +1,215 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RdCertImport
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mfbChoice_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Multiselect = false,
+                CheckFileExists = true,
+                DefaultExt = "pfx",
+                Filter = "PFX(*.pfx)|*.pfx|CRT(*.crt)|*.crt|All files (*.*)|*.*"
+            };
+            var res = ofd.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                CertFile = ofd.FileName;
+                materialSingleLineTextField1.Text = ofd.SafeFileName;
+                clearerror();
+            }
+        }
+        private void clearerror()
+        {
+            errorProvider1.Clear();
+        }
+        public string CertFile { get; set; }
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+            if (CheckFileExist())
+            {
+                try
+                {
+                    X509Certificate2 cert = new X509Certificate2(CertFile, materialSingleLineTextField2.Text);
+                    var hash = cert.GetCertHashString();
+                    MessageBox.Show($"密码正确,哈希为:{hash}");
+                }
+                catch (System.Security.Cryptography.CryptographicException ex)
+                {
+
+                    errorProvider1.SetError(materialSingleLineTextField2, ex.Message);
+
+                }
+            }
+        }
+
+        public bool CheckFileExist()
+        {
+            clearerror();
+            if (!File.Exists(CertFile))
+            {
+                errorProvider1.SetError(materialSingleLineTextField1, "文件不存在");
+                return false;
+            }
+            return true;
+        }
+
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            if (CheckFileExist())
+            {
+                X509Store store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+                string hash = null;
+                try
+                {
+
+                    store.Open(OpenFlags.ReadWrite);
+                    X509Certificate2 cert = new X509Certificate2(CertFile, materialSingleLineTextField2.Text, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
+
+                    hash = cert.GetCertHashString();
+                    if (store.Certificates.Contains(cert))
+                    {
+                        //errorProvider1.SetError(materialRaisedButton1, "已经导入");
+
+                        var choice = MessageBox.Show("已经存在该证书，是否重新导入\n选择是重新导入，选择否直接设置，选择取消终止操作", "证书已被导入", MessageBoxButtons.YesNoCancel);
+                        if (choice == DialogResult.Cancel)
+                        {
+                            return;
+                        }
+                        if (choice == DialogResult.Yes)
+                        {
+                            store.Certificates.Remove(cert);
+                            store.Add(cert);
+                        }
+                    }
+                    else
+                    {
+                        store.Add(cert);
+                    }
+
+
+                    if (hash != null)
+                    {
+                        SetRdCertByHash(hash);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errorProvider1.SetError(materialRaisedButton1, ex.Message);
+                }
+                finally
+                {
+                    store.Close();
+                }
+
+            }
+        }
+
+        private bool SetRdCertByHash(string hash)
+        {
+            var args = $@"/namespace:\\root\cimv2\TerminalServices PATH Win32_TSGeneralSetting Set SSLCertificateSHA1Hash=""{hash}""";
+
+            Process ps = new Process();
+            ps.StartInfo = new ProcessStartInfo("wmic", args)
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true
+            };
+            ps.Start();
+            ps.WaitForExit(10000);
+            if (!ps.HasExited)
+            {
+                errorProvider1.SetError(materialRaisedButton1, "超时");
+                return false;
+            }
+            else
+            {
+                if (ps.ExitCode == 0)
+                {
+                    MessageBox.Show("操作成功，请重新连接远程桌面查看结果");
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show($"可能出现了一些意外……\n详细信息：\n{ps.StandardOutput.ReadToEnd()}");
+                    return false;
+                }
+            }
+        }
+
+        private void materialRaisedButton2_Click(object sender, EventArgs e)
+        {
+            if (!SetRdCertByHash(certHash.Text))
+            {
+                MessageBox.Show("或许是Hash不正确");
+            }
+        }
+
+        private void materialFlatButton2_Click(object sender, EventArgs e)
+        {
+            if (CheckFileExist())
+            {
+                try
+                {
+                    X509Certificate2 cert = new X509Certificate2(CertFile, materialSingleLineTextField2.Text);
+                    var hash = cert.GetCertHashString();
+                    Clipboard.SetText(hash);
+                    MessageBox.Show("已复制到剪切板");
+                }
+                catch (System.Security.Cryptography.CryptographicException ex)
+                {
+
+                    errorProvider1.SetError(materialSingleLineTextField2, ex.Message);
+
+                }
+            }
+
+        }
+
+        private void materialSingleLineTextField1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialSingleLineTextField1_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+        }
+    }
+}
